e36a2261039c4b9a86891d0f48101769d299be45
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2021-04-06 15:46:18 +0100

    test_utils: TestFlowFromMatrix implemented

--- test/test_utils.py
+++ test/test_utils.py
@@ -12,7 +12,10 @@
 
 import torch
 import unittest
-from oflibpytorch.utils import get_valid_ref, get_valid_padding, validate_shape, get_valid_device
+import numpy as np
+import math
+from oflibpytorch.utils import get_valid_ref, get_valid_padding, validate_shape, get_valid_device, to_numpy, \
+    flow_from_matrix
 
 
 class TestValidityChecks(unittest.TestCase):
@@ -59,6 +62,80 @@ class TestValidityChecks(unittest.TestCase):
             validate_shape([-1, 10])
         with self.assertRaises(ValueError):
             validate_shape([10., 10])
+
+
+class TestFlowFromMatrix(unittest.TestCase):
+    # All numerical values in calculated manually and independently
+    def test_identity(self):
+        # No transformation, equals passing identy matrix, to 200 by 300 flow field
+        shape = [200, 300]
+        matrix = torch.eye(3)
+        flow = flow_from_matrix(matrix, shape)
+        self.assertIsNone(np.testing.assert_equal(to_numpy(flow), 0))
+        self.assertIsNone(np.testing.assert_equal(flow.shape[1:], shape))
+
+    def test_translation(self):
+        # Translation of 10 horizontally, 20 vertically, to 200 by 300 flow field
+        shape = [200, 300]
+        matrix = torch.tensor([[1, 0, 10], [0, 1, 20], [0, 0, 1]])
+        flow = flow_from_matrix(matrix, shape)
+        self.assertIsNone(np.testing.assert_equal(to_numpy(flow[0]), 10))
+        self.assertIsNone(np.testing.assert_equal(to_numpy(flow[1]), 20))
+        self.assertIsNone(np.testing.assert_equal(flow.shape[1:], shape))
+
+    def test_rotation(self):
+        # Rotation of 30 degrees counter-clockwise, to 200 by 300 flow field
+        shape = [200, 300]
+        matrix = torch.tensor([[math.sqrt(3) / 2, .5, 0], [-.5, math.sqrt(3) / 2, 0], [0, 0, 1]])
+        flow = flow_from_matrix(matrix, shape)
+        self.assertIsNone(np.testing.assert_equal(to_numpy(flow[:, 0, 0]), [0, 0]))
+        self.assertIsNone(np.testing.assert_allclose(to_numpy(flow[:, 0, 299]), [-40.0584042685, -149.5], rtol=1e-6))
+        self.assertIsNone(np.testing.assert_allclose(to_numpy(flow[:, 199, 0]), [99.5, -26.6609446469], rtol=1e-6))
+        self.assertIsNone(np.testing.assert_equal(flow.shape[1:], shape))
+
+    def test_rotation_with_shift(self):
+        # Rotation of 30 degrees clockwise around point [10, 50] (hor, ver), to 200 by 300 flow field
+        shape = [200, 300]
+        matrix = torch.tensor([[math.sqrt(3) / 2, -.5, 26.3397459622],
+                               [.5, math.sqrt(3) / 2, 1.69872981078],
+                               [0, 0, 1]])
+        flow = flow_from_matrix(matrix, shape)
+        self.assertIsNone(np.testing.assert_array_almost_equal(to_numpy(flow[:, 50, 10]), [0, 0]))
+        self.assertIsNone(np.testing.assert_allclose(to_numpy(flow[:, 50, 299]), [-38.7186583063, 144.5], rtol=1e-6))
+        self.assertIsNone(np.testing.assert_allclose(to_numpy(flow[:, 199, 10]), [-74.5, -19.9622148361], rtol=1e-6))
+        self.assertIsNone(np.testing.assert_equal(flow.shape[1:], shape))
+
+    def test_scaling(self):
+        # Scaling factor 0.8, to 200 by 300 flow field
+        shape = [200, 300]
+        matrix = torch.tensor([[.8, 0, 0], [0, .8, 0], [0, 0, 1]])
+        flow = flow_from_matrix(matrix, shape)
+        self.assertIsNone(np.testing.assert_equal(to_numpy(flow[:, 0, 0]), [0, 0]))
+        self.assertIsNone(np.testing.assert_allclose(to_numpy(flow[:, 0, 100]), [-20, 0]))
+        self.assertIsNone(np.testing.assert_allclose(to_numpy(flow[:, 100, 0]), [0, -20]))
+        self.assertIsNone(np.testing.assert_equal(flow.shape[1:], shape))
+
+    def test_scaling_with_shift(self):
+        # Scaling factor 2 around point [20, 30] (hor, ver), to 200 by 300 flow field
+        shape = [200, 300]
+        matrix = torch.tensor([[2, 0, -20], [0, 2, -30], [0, 0, 1]])
+        flow = flow_from_matrix(matrix, shape)
+        self.assertIsNone(np.testing.assert_array_almost_equal(to_numpy(flow[:, 30, 20]), [0, 0]))
+        self.assertIsNone(np.testing.assert_allclose(to_numpy(flow[:, 30, 70]), [50, 0]))
+        self.assertIsNone(np.testing.assert_allclose(to_numpy(flow[:, 80, 20]), [0, 50]))
+        self.assertIsNone(np.testing.assert_equal(flow.shape[1:], shape))
+
+    def test_device(self):
+        device = ['cpu', 'cuda']
+        if torch.cuda.is_available():
+            expected_device = ['cpu', 'cuda']
+        else:
+            expected_device = ['cpu', 'cpu']
+        shape = [200, 300]
+        matrix = torch.eye(3)
+        for dev, expected_dev in zip(device, expected_device):
+            flow = flow_from_matrix(matrix.to(dev), shape)
+            self.assertEqual(flow.device.type, expected_dev)
 
 
 if __name__ == '__main__':
