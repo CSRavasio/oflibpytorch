4498c82c26705cf6317fe4fdbe1463a19a269570
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2021-05-04 12:36:33 +0100

    License trim, file intro docstring adjustment

--- test/test_flow_class.py
+++ test/test_flow_class.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright: 2021, Claudio S. Ravasio
-# License: LGPL 2.1
-# Author: Claudio S. Ravasio, PhD student at University College London (UCl), supervised by:
+# License: GPL 3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+# Author: Claudio S. Ravasio, PhD student at University College London (UCL), research assistant at King's College
+# London (KCL), supervised by:
 #   Dr Christos Bergeles, PI of the Robotics and Vision in Medicine (RViM) lab in the School of Biomedical Engineering &
 #       Imaging Sciences (BMEIS) at King's College London (KCL)
 #   Prof Lyndon Da Cruz, consultant ophthalmic surgeon, Moorfields Eye Hospital, London UK
