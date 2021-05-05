43470290da0de794e72334e26dac273414a1c479
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2021-05-05 17:25:10 +0100

    License change

--- test/test_utils.py
+++ test/test_utils.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright: 2021, Claudio S. Ravasio
-# License: GPL 3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+# License: MIT (https://opensource.org/licenses/MIT)
 # Author: Claudio S. Ravasio, PhD student at University College London (UCL), research assistant at King's College
 # London (KCL), supervised by:
 #   Dr Christos Bergeles, PI of the Robotics and Vision in Medicine (RViM) lab in the School of Biomedical Engineering &
