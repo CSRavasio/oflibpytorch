8700e0e90d097520488107d57ae51a102b456b60
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2021-05-28 18:22:28 +0100

    __init__.py added

---
+++ oflibpytorch/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright: 2021, Claudio S. Ravasio
+# License: MIT (https://opensource.org/licenses/MIT)
+# Author: Claudio S. Ravasio, PhD student at University College London (UCL), research assistant at King's College
+# London (KCL), supervised by:
+#   Dr Christos Bergeles, PI of the Robotics and Vision in Medicine (RViM) lab in the School of Biomedical Engineering &
+#       Imaging Sciences (BMEIS) at King's College London (KCL)
+#   Prof Lyndon Da Cruz, consultant ophthalmic surgeon, Moorfields Eye Hospital, London UK
+#
+# This file is part of oflibpytorch
+
+from .flow_class import Flow
+from .flow_operations import *
