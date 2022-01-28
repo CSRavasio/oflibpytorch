e023d1e01c30c4b27460745150802950771d3d40
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2022-01-28 15:19:51 +0000

    utils: normalise_coords potential error source fixed

--- src/oflibpytorch/utils.py
+++ src/oflibpytorch/utils.py
@@ -359,7 +359,7 @@ def normalise_coords(coords: torch.Tensor, shape: Union[tuple, list]) -> torch.T
     :param shape: list of flow (or image) size [ver, hor]
     :return: Normalised coordinates
     """
-    normalised_coords = 2. * coords
+    normalised_coords = coords.float() * 2
     normalised_coords[..., 0] /= (shape[1] - 1)  # points[..., 0] is x, which is horizontal, so shape[1]
     normalised_coords[..., 1] /= (shape[0] - 1)  # points[..., 1] is y, which is vertical, so shape[0]
     normalised_coords -= 1
