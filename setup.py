2d87f1217bf3961ec44ee84382bb3258ce4ecf4a
Claudio Ravasio <CSRavasio@users.noreply.github.com>
2022-06-21 11:34:00 +0100

    Setup version number updated to 2.1.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ print(setup_message)
 
 setup(
     name='oflibpytorch',
-    version='2.0.0',
+    version='2.1.0',
     description='Differentiable optical flow library using a custom flow class based on PyTorch tensors',
     long_description=long_description,
     long_description_content_type='text/x-rst',
